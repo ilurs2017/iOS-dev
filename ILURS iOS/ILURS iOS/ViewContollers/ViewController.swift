3b2ec9e978f71c3ecee4b3c3307fc825b4d6a18f
Carin Gan <yezhuan@wharton.upenn.edu>
2018-05-06 17:51:24 -0400

    Add files via upload

---
+++ ILURS iOS/ILURS iOS/ViewContollers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ILURS iOS
+//
+//  Created by Carin Gan on 5/6/18.
+//  Copyright © 2018 Carin Gan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
